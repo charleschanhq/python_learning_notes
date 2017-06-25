52c1a79d75f73c8524758edd1549468b1dda234a
CharlesChan <charleschanhq@gmail.com>
2017-06-25 01:43:05 -0400

    Hanqing
    python' function' notes

--- pythonlearning/learning.py
+++ pythonlearning/learning.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-
+import math
 '''
 
 number=input("please enter a number: ")
@@ -110,8 +110,31 @@ print("Is space a space :", a_space.isspace())
 '''
 
 
+'''
+# check if a number if a prime number
+def isprime(num):
+
+    for i in range(2, num):
+        if (num % i) == 0:
+            return False
+    return True
 
 
+def getPrimes(max_number):
+    list_of_primes = []
+    for num1 in range(2, max_number):
+        if isprime(num1):
+            list_of_primes.append(num1)
+    return list_of_primes
+
+max_num_to_check = int(input("Search for Primes up to : "))
+
+list_of_primes = getPrimes(max_num_to_check)
+
+for prime in list_of_primes:
+    print(prime)
+
+'''
 
 
 
