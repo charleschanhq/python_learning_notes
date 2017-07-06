0cbd15ce5880d0ff43db327182305720ebb7d0d8
CharlesChan <charleschanhq@gmail.com>
2017-07-06 02:07:47 -0400

    Hanqing

--- pythonlearning/learning.py
+++ pythonlearning/learning.py
@@ -382,7 +382,7 @@ print(fib(7))
 
 
 
-
+'''
 # reading and writing files
 
 with open("newfile.txt",mode='w',encoding='utf-8') as myfile:
@@ -407,3 +407,103 @@ print('current Directory:', os.getcwd())
 print('current Directory:', os.getcwd())
 # rmdir will remove the dir
 os.rmdir('mydir')
+
+
+def fib(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        result = fib(num - 1) + fib(num - 2)
+        return result
+
+numFibValues = int(input("How many Fibonacci values should be found : "))
+
+i = 1
+
+# While i is less then the number of values requested
+# continue to find more
+while i < numFibValues:
+    # Call the fib()
+    fibValue = fib(i)
+    print(fibValue)
+    i += 1
+print("All Done")
+
+# ---------- READ ONE LINE AT A TIME ----------
+# You can read 1 line at a time with readline()
+# Open the file
+with open("mydata2.txt", encoding="utf-8") as myFile:
+    lineNum = 1
+    # We'll use a while loop that loops until the data
+    # read is empty
+    while True:
+        line = myFile.readline()
+        # line is empty so exit
+        if not line:
+            break
+        print("Line", lineNum, " :", line, end="")
+        lineNum += 1
+# ---------- PROBLEM : ANALYZE THE FILE ----------
+# As you cycle through each line output the number of
+# words and average word length
+'''
+#Line 1
+#Number of Words : 3
+#Avg Word Length : 4.7
+#Line 2
+#Number of Words : 3
+#Avg Word Length : 4.7
+'''
+with open("mydata2.txt", encoding="utf-8") as myFile:
+    lineNum = 1
+    while True:
+        line = myFile.readline()
+        # line is empty so exit
+        if not line:
+            break
+        print("Line", lineNum)
+        # Put the words in a list using the space as
+        # the boundary between words
+        wordList = line.split()
+        # Get the number of words with len()
+        print("Number of Words :", len(wordList))
+        # Incremented for each character
+        charCount = 0
+        for word in wordList:
+            for char in word:
+                charCount += 1
+        # Divide to find the answer
+        avgNumChars = charCount / len(wordList)
+        # Use format to limit to 2 decimals
+        print("Avg Word Length : {:.2}".format(avgNumChars))
+        lineNum += 1
+
+'''
+
+
+
+
+
+
+
+
+#tuple
+newtuple=(1,2,4,5,6,8)
+print (newtuple[0])
+print(newtuple[0:4])
+print(len(newtuple))
+mynewtuple=newtuple+(9,10)
+print(mynewtuple)
+print(10 in newtuple)
+print(max(newtuple))
+print(min(newtuple))
+
+
+
+
+
+# object oriented programmming
+
+
